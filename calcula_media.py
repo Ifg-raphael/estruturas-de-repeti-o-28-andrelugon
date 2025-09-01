597e06563a18492430ae6a6658308446a3e033ca
andrelugon <andreluiz.lugon@gmail.com>
2025-09-01 20:52:02 -0300

    Resolução do exercício 28 da lista 04

--- calcula_media.py
+++ calcula_media.py
@@ -1,1 +1,15 @@
-# Sua solução aqui
+#Programa recebe cinco valores e calcula a média entre esse valores:
+
+#Entrada de dados: O usuário informa os valores n1, n2, n3, n4 e n5
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+n4 = int(input())
+n5 = int(input())
+
+#Processamento: Cálculo da média dos valores informados
+media = (n1 + n2 + n3 + n4 + n5) / 5
+
+#Saída de dados: Imprime o valor da média
+print(f"{media}")
+
