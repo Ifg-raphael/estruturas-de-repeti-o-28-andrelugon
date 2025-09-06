f58f55ae2d52fe99ddc01c3399e07c9c4769f44c
andrelugon <andreluiz.lugon@gmail.com>
2025-09-05 21:16:37 -0300

    altera a entrada de dados de int para float

--- calcula_media.py
+++ calcula_media.py
@@ -1,15 +1,15 @@
 #Programa recebe cinco valores e calcula a média entre esse valores:
 
 #Entrada de dados: O usuário informa os valores n1, n2, n3, n4 e n5
-n1 = int(input())
-n2 = int(input())
-n3 = int(input())
-n4 = int(input())
-n5 = int(input())
+n1 = float(input())
+n2 = float(input())
+n3 = float(input())
+n4 = float(input())
+n5 = float(input())
 
 #Processamento: Cálculo da média dos valores informados
 media = (n1 + n2 + n3 + n4 + n5) / 5
 
 #Saída de dados: Imprime o valor da média
-print(f"{media}")
+print(f"{media:.2f}")
 
